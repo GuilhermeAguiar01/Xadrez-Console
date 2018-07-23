28ae89bb995e795d86f8151215def9bf89397f44
Guilherme Aguiar <guilherme.aguiar@rocketmail.com>
2018-07-23 15:44:34 -0300

    Criei as Classes Cor, Peca e Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
